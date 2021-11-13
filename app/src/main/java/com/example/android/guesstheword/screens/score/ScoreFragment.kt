82c8a39306e5e15d4923faf3ee49e42c19de53da
agiltandiera <agiltandiera27@gmail.com>
2021-11-13 22:05:57 +0800

    use a ViewModelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -31,6 +31,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  */
 class ScoreFragment : Fragment() {
 
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -44,6 +47,11 @@ class ScoreFragment : Fragment() {
                 container,
                 false
         )
+
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(ScoreViewModel::class.java)
+        binding.scoreText.text = viewModel.score.toString()
 
         return binding.root
     }
--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -1,4 +1,10 @@
 package com.example.android.guesstheword.screens.score
 
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+            return ScoreViewModel(finalScore) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
