cabf5d5dbb564024485a5fcd43cdf60bba8b9280
agiltandiera <agiltandiera27@gmail.com>
2021-11-13 21:27:59 +0800

    add an init block in GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,4 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
-class GameViewModel {
+class GameViewModel : ViewModel() {
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
 }
