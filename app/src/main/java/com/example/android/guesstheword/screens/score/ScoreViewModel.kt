a5887a36f253aafa680549d3215fcda2c51a3b2e
agiltandiera <agiltandiera27@gmail.com>
2021-11-13 22:01:56 +0800

    create ScoreViewModelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.android.guesstheword.screens.score
 
-class ScoreViewModel {
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    // The final score
+    var score = finalScore
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
 }
---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.example.android.guesstheword.screens.score
+
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+}
