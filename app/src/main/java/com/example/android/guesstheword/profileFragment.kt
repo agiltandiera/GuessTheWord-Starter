95d6b4a75be1355cf3281558909d900adeb5fa00
agiltandiera <agiltandiera27@gmail.com>
2021-11-16 21:20:40 +0800

    add menu

--- app/src/main/java/com/example/android/guesstheword/profileFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/profileFragment.kt
@@ -11,6 +11,6 @@ class profileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
